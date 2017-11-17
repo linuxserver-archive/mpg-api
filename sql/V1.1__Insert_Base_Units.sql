de5646d1f55e2d587d2557102ea516b134cd5cb6
Josh Stark <jagfin1@gmail.com>
2017-11-17 13:21:06 +0000

    Added base units for SQL

---
+++ sql/V1.1__Insert_Base_Units.sql
@@ -0,0 +1,21 @@
+delimiter $$
+
+INSERT INTO `fuelUnits` (`unit`, `unitName`) VALUES ("l", "Litres")$$
+INSERT INTO `fuelUnits` (`unit`, `unitName`) VALUES ("gal", "US Gallon")$$
+INSERT INTO `fuelUnits` (`unit`, `unitName`) VALUES ("gal", "UK Gallon")$$
+INSERT INTO `fuelUnits` (`unit`, `unitName`) VALUES ("kWh", "Kilowatt Hour")$$
+
+INSERT INTO `fuelTypes` (`type`) VALUES ("Petrol")$$
+INSERT INTO `fuelTypes` (`type`) VALUES ("Diesel")$$
+INSERT INTO `fuelTypes` (`type`) VALUES ("Ethanol")$$
+INSERT INTO `fuelTypes` (`type`) VALUES ("LPG")$$
+INSERT INTO `fuelTypes` (`type`) VALUES ("Electic")$$
+
+INSERT INTO `distanceUnits` (`unit`, `unitName`) VALUES ("mi", "Miles")$$
+INSERT INTO `distanceUnits` (`unit`, `unitName`) VALUES ("km", "Kilometres")$$
+
+INSERT INTO `fuelConsumptionUnits` (`unit`, `unitName`) VALUES ("mpg", "Miles per Gallon")$$
+INSERT INTO `fuelConsumptionUnits` (`unit`, `unitName`) VALUES ("km/l", "Kilometres per Litre")$$
+INSERT INTO `fuelConsumptionUnits` (`unit`, `unitName`) VALUES ("mi/l", "Miles per Litre")$$
+INSERT INTO `fuelConsumptionUnits` (`unit`, `unitName`) VALUES ("kWh/mi", "Kilowatt Hours per Mile")$$
+INSERT INTO `fuelConsumptionUnits` (`unit`, `unitName`) VALUES ("kWh/km", "Kilowatt Hours per Kilometre")$$
--- sql/V1__Create_Tables.sql
+++ sql/V1__Create_Tables.sql
@@ -1,40 +1,80 @@
 delimiter $$
 
+CREATE TABLE `fuelUnits` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `unit` VARCHAR(20) NOT NULL,
+    `unitName` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `id_UNIQUE` (`id` ASC)
+)$$
+
+CREATE TABLE `fuelConsumptionUnits` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `unit` VARCHAR(20) NOT NULL,
+    `unitName` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `id_UNIQUE` (`id` ASC)
+)$$
+
+CREATE TABLE `distanceUnits` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `unit` VARCHAR(20) NOT NULL,
+    `unitName` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `id_UNIQUE` (`id` ASC)
+)$$
+
+CREATE TABLE `fuelTypes` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `type` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `id_UNIQUE` (`id` ASC)
+)$$
+
 CREATE TABLE `vehicles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `registration` varchar(20) DEFAULT NULL,
-  `make` varchar(100) DEFAULT NULL,
-  `model` varchar(100) DEFAULT NULL,
-  `yearOfManufacture` INT(11) NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) NOT NULL,
+    `registration` varchar(20) DEFAULT NULL,
+    `make` varchar(100) DEFAULT NULL,
+    `model` varchar(100) DEFAULT NULL,
+    `yearOfManufacture` INT(11) NULL,
+    `vin` VARCHAR(100) NULL,
+    `fuelType` INT(11) NOT NULL,
+    `fuelUnit` INT(11) NOT NULL,
+    `distanceUnit` INT(11) NOT NULL,
+    `fuelConsumptionUnit` INT(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `id_UNIQUE` (`id` ASC),
+    INDEX `FK_vehicle_fuelUnit_idx` (`fuelUnit`),
+    INDEX `FK_vehicle_fuelType_idx` (`fuelType`),
+    INDEX `FK_vehicle_distanceUnit_idx` (`distanceUnit`),
+    INDEX `FK_vehicle_fuelConsumptionUnit_idx` (`fuelConsumptionUnit`),
+    CONSTRAINT `FK_vehicle_fuelType` FOREIGN KEY (`fuelType`) REFERENCES `fuelTypes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT `FK_vehicle_fuelUnit` FOREIGN KEY (`fuelUnit`) REFERENCES `fuelUnits` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT `FK_vehicle_distanceUnit` FOREIGN KEY (`distanceUnit`) REFERENCES `distanceUnits` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT `FK_vehicle_fuelConsumptionUnit` FOREIGN KEY (`fuelConsumptionUnit`) REFERENCES `fuelConsumptionUnits` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 )$$
 
 CREATE  TABLE `fuel` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `vehicleId` INT(11) NOT NULL,
-  `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `fuelAmountLitres` DECIMAL(5,2) NOT NULL,
-  `totalCost` DECIMAL(5,2) NULL,
-  `fuelCostPerLitre` DECIMAL(5,2) NULL,
-  `locationLatitude` DECIMAL(10,8) NULL,
-  `locationLongitude` DECIMAL(11,8) NULL,
-  `odometerReading` INT(11) NULL,
-  `notes` TEXT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `FK_fuel_vehicleId_idx` (`vehicleId` ASC),
-  CONSTRAINT `FK_fuel_vehicleId`
-    FOREIGN KEY (`vehicleId`)
-    REFERENCES `vehicles` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `vehicle` INT(11) NOT NULL,
+    `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `fuelAmountLitres` DECIMAL(5,2) NOT NULL,
+    `totalCost` DECIMAL(5,2) NULL,
+    `fuelCostPerLitre` DECIMAL(5,2) NULL,
+    `locationLatitude` DECIMAL(10,8) NULL,
+    `locationLongitude` DECIMAL(11,8) NULL,
+    `odometerReading` INT(11) NULL,
+    `notes` TEXT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+    INDEX `FK_fuel_vehicleId_idx` (`vehicle` ASC),
+    CONSTRAINT `FK_fuel_vehicleId` FOREIGN KEY (`vehicle`) REFERENCES `vehicles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 )$$
 
 CREATE TABLE `cost` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `vehicleId` INT(11) NOT NULL,
+    `vehicle` INT(11) NOT NULL,
     `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `costType` VARCHAR(20) NOT NULL,
     `totalCost` DECIMAL(6,2) NOT NULL,
@@ -42,10 +82,6 @@ CREATE TABLE `cost` (
     `notes` TEXT NULL,
     PRIMARY KEY (`id`),
     UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-    INDEX `FK_cost_vehicleId_idx` (`vehicleId` ASC),
-    CONSTRAINT `FK_cost_vehicleId`
-      FOREIGN KEY (`vehicleId`)
-      REFERENCES `vehicles` (`id`)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
+    INDEX `FK_cost_vehicleId_idx` (`vehicle` ASC),
+    CONSTRAINT `FK_cost_vehicleId` FOREIGN KEY (`vehicle`) REFERENCES `vehicles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 )$$
